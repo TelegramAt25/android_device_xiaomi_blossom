06d607b0c6bd6fa785398e134935a1093053a5c7
xNombre <kartapolska@gmail.com>
2023-03-05 20:30:00 +0530

    blossom: Enable IncrementalFS support
     * Since IncFS v2 was backported for 4.14 users can now benefit from faster
       Play Store app downloads
    
    [fukiame: blossom amends: we are 4.19
    welp, still sameh]
    
    Signed-off-by: neilchetty <neilchetty4559@gmail.com>
    Change-Id: I0f5bd4410580bb2e30defd8275b9902bc2f9d060

--- device.mk
+++ device.mk
@@ -141,6 +141,10 @@ PRODUCT_BOOT_JARS += \
     mediatek-telephony-base \
     mediatek-telephony-common
 
+# IncFS
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.incremental.enable=yes
+
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/permissions/privapp-permissions-com.mediatek.ims.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-com.mediatek.ims.xml
 
