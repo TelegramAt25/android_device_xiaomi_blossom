7a87e044e887a3518fd6fa32bc4a52fd59f8d671
Reinazhard <reinazhard@gmail.com>
2023-05-05 04:36:05 +0200

    blossom: Import Android Go performance tuning
    Signed-off-by: Reinazhard <reinazhard@gmail.com>

--- device.mk
+++ device.mk
@@ -412,6 +412,14 @@ PRODUCT_USE_PROFILE_FOR_BOOT_IMAGE := true
 USE_DEX2OAT_DEBUG := false
 WITH_DEXPREOPT_DEBUG_INFO := false
 
+# Do not generate libartd.
+PRODUCT_ART_TARGET_INCLUDE_DEBUG_BUILD := false
+
+# Strip the local variable table and the local variable type table to reduce
+# the size of the system image. This has no bearing on stack traces, but will
+# leave less information available via JDWP.
+PRODUCT_MINIMIZE_JAVA_DEBUG_INFO := true
+
 PRODUCT_DEXPREOPT_SPEED_APPS += \
     Settings
 
