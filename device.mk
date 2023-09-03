32f474b36915cd4d752c52642a5dcb3e7388764b
bengris32 <bengris32@protonmail.ch>
2023-09-03 20:45:29 +0530

    blossom: Source build mtkpower interfaces
    Signed-off-by: bengris32 <bengris32@protonmail.ch>
    Change-Id: I1365b80314604530d9e7c7be1acca7991c6fd7dc

--- device.mk
+++ device.mk
@@ -240,6 +240,10 @@ PRODUCT_PACKAGES += \
     libmtkperf_client_vendor \
     libmtkperf_client
 
+PRODUCT_PACKAGES += \
+    vendor.mediatek.hardware.mtkpower@1.0.vendor \
+    vendor.mediatek.hardware.mtkpower@1.1.vendor
+
 # Health
 PRODUCT_PACKAGES += \
     android.hardware.health@2.1-impl \
