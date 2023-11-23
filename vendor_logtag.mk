7b893f940a7cd65218095fde947b16e687806837
alibei <sebastiankotzias83@gmail.com>
2023-11-23 16:12:41 +0000

    blossom: Silence SpeechMessengerNormal logspam
    SpeechMessengerNormal: openCcciDriver(), open(/dev/ccci_aud) failgit add . mCcciDeviceHandler: -1, errno: 2
    
    Signed-off-by: 01soni247 <01soni247@gmail.com>

--- vendor_logtag.mk
+++ vendor_logtag.mk
@@ -249,6 +249,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
     persist.log.tag.SimSwitchOP02=$(VENDOR_LOG_LEVEL) \
     persist.log.tag.SimSwitchOP18=$(VENDOR_LOG_LEVEL) \
     persist.log.tag.SlotQueueEntry=$(VENDOR_LOG_LEVEL) \
+    persist.log.tag.SpeechMessenger=$(VENDOR_LOG_LEVEL) \
     persist.log.tag.SpnOverride=$(VENDOR_LOG_LEVEL) \
     persist.log.tag.UiccCard=$(VENDOR_LOG_LEVEL) \
     persist.log.tag.UiccController=$(VENDOR_LOG_LEVEL) \
