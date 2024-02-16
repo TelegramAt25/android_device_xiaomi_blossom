1cca4546bdc182349d03025a05033c2c700a76e9
Jens Reidel <adrian@travitia.xyz>
2024-02-16 19:45:53 +0000

    blossom: Explicitly disable serial console
    While it should already be disabled, the system searches for
    "console=null" in cmdline to make sure that the serial console is
    disabled. Add this part to make the permanent notification disappear.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -98,6 +98,7 @@ BOARD_KERNEL_CMDLINE += cgroup_disable=pressure
 BOARD_KERNEL_CMDLINE += cgroup.memory=nokmem,nosocket
 BOARD_KERNEL_CMDLINE += nodebugmon
 BOARD_KERNEL_CMDLINE += noirqdebug
+BOARD_KERNEL_CMDLINE += console=null
 
 # Partitions
 BOARD_FLASH_BLOCK_SIZE := 131072 # (BOARD_KERNEL_PAGESIZE * 64)
