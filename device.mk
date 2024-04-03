b83a4d40580946ea53503cd431acb8b183aa15f9
Sarthak Roy <sarthakroy2002@gmail.com>
2024-04-03 21:21:10 +0530

    blossom: Build power-service.blossom-libperfmgr
    Signed-off-by: Sarthak Roy <sarthakroy2002@gmail.com>
    Change-Id: I37cff12b773ab4c5b4b9da80ba03725f5afb3fe7
    Signed-off-by: Saikrishna1504 <saikrishna26918@gmail.com>

--- device.mk
+++ device.mk
@@ -268,7 +268,7 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power-service.mediatek-libperfmgr \
+    android.hardware.power-service.blossom-libperfmgr \
     android.hardware.power@1.0.vendor \
     android.hardware.power@1.1.vendor \
     android.hardware.power@1.2.vendor \
