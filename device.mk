8b115df9d265da2eb95518885c5d2e40f8cff730
Yumi Yukimura <me.cafebabe@gmail.com>
2024-04-14 17:19:24 +0530

    blossom: Switch to USB 1.3-basic HAL
    https://github.com/LineageOS/android_hardware_lineage_interfaces/commit/24bb38a49928daa9ac1015e5978dee0cc5fc0ef8
    
    FIXES: Offending entry: android.hardware.usb@1.0-service.basic
    Change-Id: I6f477db2220b3563b44bb9545b7889166b8a6b06
    Signed-off-by: Omansh Krishn <omansh11597@gmail.com>

--- device.mk
+++ device.mk
@@ -96,7 +96,7 @@ PRODUCT_PACKAGES += \
 
 # USB
 PRODUCT_PACKAGES += \
-    android.hardware.usb@1.0-service.basic \
+    android.hardware.usb@1.3-service.basic \
     android.hardware.usb@1.0.vendor:64
 
 # Audio
