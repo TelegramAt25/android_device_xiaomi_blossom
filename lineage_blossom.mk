b9de150b65fbd6b8ce6273486f66c82ed2f825f5
fukiame <fukiame@proton.me>
2024-04-23 06:00:05 +0700

    Revert "blossom: LeafOS bringup"
    This reverts commit ebf34fee9fe39d85bc964ae73938479ffc56d642.

--- lineage_blossom.mk
+++ lineage_blossom.mk
@@ -12,10 +12,10 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/non_ab_device.mk)
 # Inherit from device makefile.
 $(call inherit-product, device/xiaomi/blossom/device.mk)
 
-# Inherit some common LeafOS stuff.
-$(call inherit-product, vendor/leaf/config/common_full_phone.mk)
+# Inherit some common LineageOS stuff.
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
-PRODUCT_NAME := blossom
+PRODUCT_NAME := lineage_blossom
 PRODUCT_DEVICE := blossom
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := Redmi
