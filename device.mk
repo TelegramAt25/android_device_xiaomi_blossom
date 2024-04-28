3af55768952c974b2e707cdd2a94b91940f30707
fukiame <fukiame@proton.me>
2024-04-28 17:39:02 +0700

    fixup! blossom: actually compile carrierconfig overlay
    im retarted

--- device.mk
+++ device.mk
@@ -447,6 +447,7 @@ PRODUCT_SOONG_NAMESPACES += \
 
 # Runtime Resource Overlays
 PRODUCT_PACKAGES += \
+    CarrierConfigOverlayBlossom \
     DialerOverlayBlossom \
     FrameworksResOverlayBlossom \
     SettingsOverlayBlossom \
