763bd197a53a3f2d7e9de47c9c9e3778b68f9f71
Tim Zimmermann <tim@linux4.de>
2024-05-09 06:48:24 +0200

    blossom: Use pixel power HAL directly
    Change-Id: I65378563396d12ee76e7f2b34105988c7e66cf38

--- device.mk
+++ device.mk
@@ -283,7 +283,7 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power-service.blossom-libperfmgr \
+    android.hardware.power-service.pixel-libperfmgr \
     android.hardware.power@1.0.vendor \
     android.hardware.power@1.1.vendor \
     android.hardware.power@1.2.vendor \
