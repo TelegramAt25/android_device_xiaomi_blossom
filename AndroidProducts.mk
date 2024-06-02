278416494f056044cfbc79f3440c988f714e1509
fukiame <fukiame@proton.me>
2024-06-02 07:21:37 +0700

    blossom: miku: init
    39! :P
    
    Signed-off-by: fukiame <fukiame@proton.me>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,10 +5,4 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/bliss_blossom.mk
-
-COMMON_LUNCH_CHOICES := \
-    bliss_blossom-user \
-    bliss_blossom-userdebug \
-    bliss_blossom-eng
-
+    $(LOCAL_DIR)/miku_blossom.mk
