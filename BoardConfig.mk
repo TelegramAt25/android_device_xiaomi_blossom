fb12180a5f591bfacc47e04d5a9f5c125160712c
Bharath <bharath@fairphone.com>
2024-06-21 13:01:42 +0200

    blossom: Unset BOARD_VNDK_VERSION
    This will always be set to current for Android-P or later
    launching devices.
    
    See https://android.googlesource.com/platform/build/+/571737e.
    
    Issue: FP3-A12#6
    Change-Id: Iedf33893ac8f8c465b81a784a14d58202b977ba7

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -187,9 +187,6 @@ DEVICE_MATRIX_FILE += $(DEVICE_PATH)/compatibility_matrix.xml
 DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/manifest.xml
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += $(DEVICE_PATH)/framework_compatibility_matrix.xml
 
-# VNDK
-BOARD_VNDK_VERSION := current
-
 # Wi-Fi
 WIFI_HIDL_FEATURE_DUAL_INTERFACE := true
 
