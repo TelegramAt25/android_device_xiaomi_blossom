09ae7ebea6c9dac1ff0390edb6f619e450fbf0de
micky387 <mickaelsaibi@free.fr>
2024-07-04 20:51:08 -0400

    blossom: Build libstagefright_softomx_plugin libraries
    E vndksupport: Could not load libstagefright_softomx_plugin.so from default namespace: dlopen failed: library "libstagefright_softomx_plugin.so" not found.
    
    Change-Id: I4b21476fc04df2acc89f2a34035b1642f2176f7f

--- device.mk
+++ device.mk
@@ -137,6 +137,9 @@ PRODUCT_COPY_FILES += \
     frameworks/av/services/audiopolicy/config/usb_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/usb_audio_policy_configuration.xml \
     frameworks/av/services/audiopolicy/config/default_volume_tables.xml:$(TARGET_COPY_OUT_VENDOR)/etc/default_volume_tables.xml
 
+PRODUCT_PACKAGES += \
+    libstagefright_softomx_plugin.vendor
+
 # Lights
 PRODUCT_PACKAGES += \
     android.hardware.light-service.blossom
