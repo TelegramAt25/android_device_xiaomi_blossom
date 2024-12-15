4bb801a8652aa794f997645f044a9c1780646a5c
fukiame <fukiame@proton.me>
2024-12-16 00:22:39 +0700

    Revert "blossom: hi yaap"
    This reverts commit 0d15d782bea6784b7af7f8d12b17f95a395266d2.
    
    Signed-off-by: fukiame <fukiame@proton.me>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,4 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/yaap_blossom.mk
-
-COMMON_LUNCH_CHOICES := \
-    yaap_blossom-user \
-    yaap_blossom-userdebug \
-    yaap_blossom-eng
+    $(LOCAL_DIR)/miku_blossom.mk
