7c19fa7db06261fba9cdd486be5f7934e1122ec2
fukiame <fukiame@proton.me>
2025-02-25 16:50:22 +0700

    blossom: to lineage
    Signed-off-by: fukiame <fukiame@proton.me>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,4 +5,4 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/miku_blossom.mk
+    $(LOCAL_DIR)/lineage_blossom.mk
--- lineage_blossom.mk
+++ lineage_blossom.mk
@@ -12,8 +12,8 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/non_ab_device.mk)
 # Inherit from device makefile.
 $(call inherit-product, device/xiaomi/blossom/device.mk)
 
-# Inherit some common Miku UI stuff.
-$(call inherit-product, vendor/miku/build/product/miku_product.mk)
+# Inherit some common LineageOS stuff.
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 # include private signing keys
 -include vendor/niigo-priv/keys/keys.mk
@@ -21,7 +21,7 @@ $(call inherit-product, vendor/miku/build/product/miku_product.mk)
 # Maintainer
 MIKU_MASTER := fukiame
 
-PRODUCT_NAME := miku_blossom
+PRODUCT_NAME := lineage_blossom
 PRODUCT_DEVICE := blossom
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := Redmi
